aada838cd82577aed458253fe719f48cd56657be
rlfrahm <frahmryan@gmail.com>
2024-09-17 15:33:52 -0500

    fix: deployment issue

--- scripts/db-sqlite.js
+++ scripts/db-sqlite.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const db = new sqlite3.Database(process.env.SQLITE_DATABASE || ':memory:');
 const query = util.promisify(db.all.bind(db));
 
-const usersCreate = query('CREATE TABLE `users` (\n' +
+const usersCreate = query('CREATE TABLE IF NOT EXISTS `users` (\n' +
   '  `id` INTEGER PRIMARY KEY AUTOINCREMENT,\n' +
   '  `userId` INTEGER NOT NULL,\n' +
   '  `email` TEXT NOT NULL,\n' +
@@ -14,7 +14,7 @@ const usersCreate = query('CREATE TABLE `users` (\n' +
   ');\n'
 );
 
-const storesCreate = query('CREATE TABLE `stores` (\n' +
+const storesCreate = query('CREATE TABLE IF NOT EXISTS `stores` (\n' +
   '  `id` INTEGER PRIMARY KEY AUTOINCREMENT,\n' +
   '  `storeHash` TEXT NOT NULL,\n' +
   '  `accessToken` TEXT,\n' +
@@ -23,7 +23,7 @@ const storesCreate = query('CREATE TABLE `stores` (\n' +
   ');\n'
 );
 
-const storeUsersCreate = query('CREATE TABLE `storeUsers` (\n' +
+const storeUsersCreate = query('CREATE TABLE IF NOT EXISTS `storeUsers` (\n' +
   '  `id` INTEGER PRIMARY KEY AUTOINCREMENT,\n' +
   '  `userId` INTEGER NOT NULL,\n' +
   '  `storeHash` TEXT NOT NULL,\n' +
